703abfb4b2e9d342d81291d09ff0be1616416f36
RSGomez <steven@rsgomez.com>
2022-08-04 18:45:44 -0700

    Add multiple variables

--- Basic Javascript Programs/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Programs/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,8 @@
-window.alert('Hello World!');
-document.write('Hello World!');
+var a = '\'String theories'
+var b = a + ' and Bing cherries\''
+
+var team = 'Avengers', ironMan = 'Tony Stark', blackWidow = 'Natasha Romanoff', theHulk = 'Bruce Banner', thor = 'Thor', captainAmerica = 'Steve Rogers', hawkeye = 'Clint Barton';
+
+
+
+document.write(theHulk);
